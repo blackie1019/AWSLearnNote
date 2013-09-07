b52e7ed40102f5f1e39ac99fbc4a68a64f266e6e
blackie <blackie1019@gmail.com>
2013-09-08 05:02:01 +0800

    update icon &data.json

--- js/index.js
+++ js/index.js
@@ -9,3 +9,49 @@ function openBrowser(linkURL){
         // window.location.href=tempURL;
     }
 }
+function iconClassName(type){
+    console.log(type);
+    var iconClass="glyphicon "; 
+    switch(type){
+        case "fb":
+            iconClass+="icon-facebook";
+            break;
+        case "g+=":
+            iconClass+="icon-googleplus";
+            break;
+        case "twitter":
+            iconClass+="icon-twitter";
+            break;
+        case "word":
+            iconClass+="icon-file-word";
+            break;
+        case "excel":
+            iconClass+="icon-file-excel";
+            break;
+        case "ppt":
+            iconClass+="icon-file-powerpoint";
+            break;
+        case "pdf":
+            iconClass+="icon-file-pdf";
+            break;
+        case "html":
+            iconClass+="icon-file-xml";
+            break;
+        case "zip":
+            iconClass+="icon-file-zip";
+            break;
+        case "img":
+            iconClass+="icon-picture";
+            break;
+        case "media":
+            iconClass+="icon-youtube";
+            break;
+        case "site":
+            iconClass+="icon-globe";
+            break;
+        default:
+            iconClass+="glyphicon-file";
+            break;
+    }
+    return iconClass;
+}
